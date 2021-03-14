986af1babcc58cc70ed086fc74fa2ea61082e2dc
patel82 <chiragpatel1982.cp@gmail.com>
2021-03-14 16:14:33 +0000

    Add Reports

--- src/test/java/BDD_PracticeEbay/RunCukesTest.java
+++ src/test/java/BDD_PracticeEbay/RunCukesTest.java
@@ -5,7 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources")
+@CucumberOptions(features = "src\\test\\resources",tags = "@ShoesPrice",
+        plugin = { "html:target/cucumber-html-report",
+                    "json:target/cucumber-json-report.json",
+                    "junit:target/cucumber-junit-report.xml",
+                    "pretty:target/cucumber-pretty-report.txt"}
+
+)// feature bracket ends
 
 class RunCukesTest {
 }
